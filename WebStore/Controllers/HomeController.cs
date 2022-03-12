0b049603e3725e2c8f9fd34417cd9e1c7bede5b5
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-12 17:00:51 +0300

    Первый контроллер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() //home/index
+        {
+            return Content("Hello from first controller");
+        }
+
+        public IActionResult SecondAction(string id)
+        {
+            return Content($"Second action with parameter {id}");
+        }
+    }
+}
