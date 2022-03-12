a7e84f87bf13629a908faf5cd6982fb94010b5ed
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-12 16:51:39 +0300

    Добавление сервисов MVC и настройка главного маршрута

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+
+
 
 var config = app.Configuration;
 
-app.MapGet("/", () => config["Greetings"]);
+app.MapGet("/greetings", () => config["Greetings"]);
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
