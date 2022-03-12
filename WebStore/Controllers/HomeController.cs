30662ff02ac0430e34a30de0b4bedd21c5bf1395
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-12 17:40:42 +0300

    Представление сотрудника

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петровчи", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
         public IActionResult Index() //home/index
         {
             return View();
@@ -12,6 +19,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction(string id)
         {
             return Content($"Second action with parameter {id}");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }
