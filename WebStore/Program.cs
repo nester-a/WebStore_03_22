835454084494d92b7c1f2329569b0decd7b53ec1
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-13 21:34:22 +0300

    Конфигурируем сервер для выдачи статических файлов

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddMvc();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 var app = builder.Build();
@@ -8,6 +9,7 @@ var app = builder.Build();
 
 var config = app.Configuration;
 
+app.UseStaticFiles();
 app.MapGet("/greetings", () => config["Greetings"]);
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
