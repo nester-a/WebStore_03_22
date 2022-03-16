2ada0a1781a501aa617823b8e1373faa10da3f2d
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-16 21:08:15 +0300

    Выделение работы с сотрудниками в отдельный контроллер

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петровчи", Age = 31 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+        public IActionResult Index() => View(_employees);
+        public IActionResult Details(int id) //home/details/id
+        {
+            var employee = _employees.FirstOrDefault(empl => empl.Id == id);
+            if(employee == null) return NotFound();
+            return View(employee);
+        }
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,34 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> _employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петровчи", Age = 31 },
-            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-        };
-        public IActionResult Index() //home/index
-        {
-            return View();
-        }
-
-        public IActionResult SecondAction(string id)
-        {
-            return Content($"Second action with parameter {id}");
-        }
-
-        public IActionResult Employees()
-        {
-            return View(_employees);
-        }
-        public IActionResult Details(int id) //home/details/id
-        {
-            var employee = _employees.FirstOrDefault(empl => empl.Id == id);
-            return View(employee);
-        }
+        public IActionResult Index() => View();
     }
 }
