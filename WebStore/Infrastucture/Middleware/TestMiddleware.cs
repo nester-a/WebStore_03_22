30f28cb9ae0352ae18b885b830c83e9ffc2928a6
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-20 19:59:37 +0300

    Пример промежуточного ПО

---
+++ WebStore/Infrastucture/Middleware/TestMiddleware.cs
@@ -0,0 +1,18 @@
+﻿namespace WebStore.Infrastucture.Middleware
+{
+    public class TestMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger<TestMiddleware> logger;
+
+        public TestMiddleware(RequestDelegate next, ILogger<TestMiddleware> logger)
+        {
+            this.next = next;
+            this.logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await next(context);
+        }
+    }
+}
--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,15 +1,20 @@
+using WebStore.Infrastucture.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddMvc();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
-var app = builder.Build();
 
+
+var app = builder.Build();
 
 
 var config = app.Configuration;
 
 app.UseStaticFiles();
+app.UseRouting();
+app.UseMiddleware<TestMiddleware>();
 app.MapGet("/greetings", () => config["Greetings"]);
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
