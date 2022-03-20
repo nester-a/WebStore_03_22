ecf4f0a0c433fc22a8ff0113d787b644b4030981
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-20 19:59:37 +0300

    Пример промежуточного ПО

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -3,13 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
-var app = builder.Build();
 
+
+var app = builder.Build();
 
 
 var config = app.Configuration;
 
 app.UseStaticFiles();
+app.UseRouting();
 app.MapGet("/greetings", () => config["Greetings"]);
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
