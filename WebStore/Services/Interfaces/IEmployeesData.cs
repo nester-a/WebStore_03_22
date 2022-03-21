be737001d5efeb5e784d7f08b83a69ee52666a76
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-21 21:03:58 +0300

    Интерфейс сервиса сотрудников

---
+++ WebStore/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee employee);
+        void Update(Employee employee);
+        bool Delete(int id);
+    }
+}
