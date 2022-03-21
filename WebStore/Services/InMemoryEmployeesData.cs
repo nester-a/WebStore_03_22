62b5cfb3827af94a63418d70f4436fb2cdad33f4
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-21 21:19:38 +0300

    Сервис сотрудников

---
+++ WebStore/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,68 @@
+﻿using WebStore.Data;
+using WebStore.Models;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly ILogger<InMemoryEmployeesData> logger;
+
+        int _currentMaxId;
+
+        public InMemoryEmployeesData(ILogger<InMemoryEmployeesData> logger)
+        {
+            this.logger = logger;
+            _currentMaxId = TestData.Employees.Max(e => e.Id);
+        }
+
+        public int Add(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            if(TestData.Employees.Contains(employee)) return employee.Id;
+
+            employee.Id = ++_currentMaxId;
+            TestData.Employees.Add(employee);
+
+
+            return employee.Id;
+        }
+
+        public bool Delete(int id)
+        {
+            var db_employee = GetById(id);
+            if (db_employee is null) return false;
+
+            TestData.Employees.Remove(db_employee);
+            return true;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return TestData.Employees;
+        }
+
+        public Employee GetById(int id)
+        {
+           return TestData.Employees.SingleOrDefault(e => e.Id == id);
+        }
+
+        public void Update(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            if (TestData.Employees.Contains(employee)) return;
+
+            var db_employee = GetById(employee.Id);
+            if (db_employee is null) return;
+
+            db_employee.LastName = employee.LastName;
+            db_employee.FirstName = employee.FirstName;
+            db_employee.Patronymic = employee.Patronymic;
+            db_employee.Age = employee.Age;
+
+            //db.SaveChanges();
+        }
+    }
+}
