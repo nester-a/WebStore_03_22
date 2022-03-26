f64854aea0e7e6b2376e4072b2ede030e0b35c95
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-26 12:29:33 +0300

    Добавлен Интерфейс БлогДата

---
+++ WebStore/Services/Interfaces/IBlogsData.cs
@@ -0,0 +1,11 @@
+﻿using WebStore.Models;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IBlogsData
+    {
+        IEnumerable<Blog> GetAll();
+        Blog GetById(int id);
+        int Add(Blog blog);
+    }
+}
