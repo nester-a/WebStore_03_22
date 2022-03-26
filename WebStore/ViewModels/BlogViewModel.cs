9d5ea3d2ba08d4aafbe0a2122a89a7f68725c47b
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-26 12:11:43 +0300

    Добавлена кнопка добьавления нового блога Добавлена модель представления Блога

---
+++ WebStore/ViewModels/BlogViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore.ViewModels
+{
+    public class BlogViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string User { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public int StarsCount { get; set; }
+        public string ImgSource { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
