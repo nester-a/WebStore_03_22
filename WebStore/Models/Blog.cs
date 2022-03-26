1a0b961c724b6d01929fdc3bb3452cb4ff05644c
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-26 12:08:30 +0300

    Добавлен класс Блог

---
+++ WebStore/Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿namespace WebStore.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string User { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public int StarsCount { get; set; }
+        public string ImgSource { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
