92e0c2f6472a644e348d600141dbde3c3e65ecea
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-27 18:38:53 +0300

    Визуальные аннотации моделей-представления Сотрудника

--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,11 +1,23 @@
-﻿namespace WebStore.ViewModels
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
     }
 }
