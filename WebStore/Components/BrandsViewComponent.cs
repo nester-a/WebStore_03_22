d601a29d063ba9999597b89010b8c96a3b04a945
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-28 21:44:10 +0300

    Заготовки визуальных компонент

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
