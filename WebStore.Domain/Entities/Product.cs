1b7b833cfe82fa682c28b293c48b6854c279328e
Алексей Нестеров <nester.a.l@yandex.ru>
2022-03-30 21:36:49 +0300

    Добавление сущности товара

---
+++ WebStore.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        public int SectionId { get; set; }
+        public int? BrandId { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+    }
+}
