539d7307aa97f766bab5e3374dad2440be599c8c
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-02 20:42:37 +0300

    Добавление данныйх валидации МоделиСозданияБлога

--- WebStore/ViewModels/CreateBlogViewModel.cs
+++ WebStore/ViewModels/CreateBlogViewModel.cs
@@ -1,11 +1,25 @@
-﻿namespace WebStore.ViewModels
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
 {
     public class CreateBlogViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+
+        [Display(Name = "Заголовок")]
+        [Required(ErrorMessage = "Заголовок не указан")]
         public string Title { get; set; } = string.Empty;
+
+        [Display(Name = "Автор")]
+        [Required(ErrorMessage = "Автор не указан")]
         public string User { get; set; } = string.Empty;
+
+        [Display(Name = "Картинка")]
         public string ImgSource { get; set; } = string.Empty;
+
+        [Display(Name = "Текст")]
         public string Body { get; set; } = string.Empty;
     }
 }
