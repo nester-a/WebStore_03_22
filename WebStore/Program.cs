81f41dbfe05726e70f9d2f73d5f3146f026f5dfc
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-03 19:14:44 +0300

    Подключение контекста БД к приложению

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 using WebStore.Conventions;
+using WebStore.DAL.Context;
 using WebStore.Services;
 using WebStore.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var config = builder.Configuration;
+builder.Services.AddDbContext<WebStoreDB>(opt => 
+    opt.UseSqlServer(config.GetConnectionString("SqlServer")));
 builder.Services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
 builder.Services.AddSingleton<IBlogsData, InMemoryBlogsData>();
 builder.Services.AddSingleton<IProductData, InMemoryProductData>();
@@ -16,7 +21,6 @@ builder.Services.AddControllersWithViews(opt => opt.Conventions.Add(new TestCont
 var app = builder.Build();
 var env = app.Environment;
 
-var config = app.Configuration;
 
 #region Middleware
 
@@ -32,7 +36,6 @@ app.UseRouting();
 
 # endregion
 
-app.MapGet("/greetings", () => config["Greetings"]);
 
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
