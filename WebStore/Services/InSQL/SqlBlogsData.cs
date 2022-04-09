11d5dd45f40724f9ffec9729b4116f255c85a312
Алексей Нестеров <nester.a.l@yandex.ru>
2022-04-09 17:30:33 +0300

    Создания сервиса работы с блогами через БД

---
+++ WebStore/Services/InSQL/SqlBlogsData.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.DAL.Context;
+using WebStore.Models;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services.InSQL
+{
+    public class SqlBlogsData : IBlogsData
+    {
+        private readonly WebStoreDB db;
+        private readonly ILogger<SqlBlogsData> logger;
+
+        public SqlBlogsData(WebStoreDB db, ILogger<SqlBlogsData> logger)
+        {
+            this.db = db;
+            this.logger = logger;
+        }
+
+        public int Add(Blog blog)
+        {
+            return AddAsync(blog).Result;
+        }
+
+        public async Task<int> AddAsync(Blog blog)
+        {
+            if (blog is null) throw new ArgumentNullException(nameof(blog));
+
+            if (db.Blogs.Contains(blog)) return blog.Id;
+
+            blog.Id = db.Blogs.Max(b => blog.Id) + 1;
+
+            logger.LogInformation("Запись блога в БД...");
+            await using (await db.Database.BeginTransactionAsync())
+            {
+                db.Blogs.Add(blog);
+
+                await db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Blogs] ON");
+
+                await db.SaveChangesAsync();
+
+                await db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Blogs] OFF");
+
+                await db.Database.CommitTransactionAsync();
+            }
+            logger.LogInformation("Запись блога в БД выполнена");
+
+            return blog.Id;
+        }
+        public IEnumerable<Blog> GetAll()
+        {
+            IQueryable<Blog> query = db.Blogs;
+
+            return query;
+        }
+
+        public Blog GetById(int id)
+        {
+            Blog query = db.Blogs.SingleOrDefault(b => b.Id == id);
+            return query;
+        }
+    }
+}
